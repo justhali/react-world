4637c8bd37e160f1b9e1eee82a75667f1aa89a38
Halima <just.hvli@gmail.com>
2022-10-31 12:24:05 +0100

    feature adding style and dynamic navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import About from "./pages/About";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+      // A mettre en premier car c'est un élément principal pour la navigation
+      <BrowserRouter>
+        <Routes>
+          {/* Liens vers les pages */}
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          {/* Le path="*" fonctionne si jamais l'url ne correspond à riien de déclaré au dessus */}
+          <Route path="*" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
   );
 };
 
---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Navigation />
+            <h1>A propos</h1>
+            <br />
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit voluptas vel, saepe quia tempora suscipit quod exercitationem necessitatibus corporis distinctio tenetur maxime architecto eos dolorum veniam dignissimos maiores, obcaecati consectetur ut optio reprehenderit. Maiores velit repellat nam pariatur rem culpa molestiae itaque ut tempore voluptatibus sapiente ipsum quo amet accusantium, totam dolor? Nulla praesentium id architecto perspiciatis voluptatem quasi labore quam excepturi vel nostrum odio amet minus iusto adipisci sequi inventore sint eos esse distinctio quidem, laborum libero. Saepe suscipit soluta, tempora ex error iusto unde doloribus, reprehenderit nemo, officia ullam dolor dolore! Sed distinctio cupiditate cumque saepe. Quod consectetur suscipit et quos accusamus ab perferendis totam labore ipsam, ipsum earum neque, perspiciatis ullam sed aspernatur? Aliquam, nam. Ratione architecto itaque nostrum magni rerum fugit odio. Sequi illum numquam dignissimos deserunt, iste non! Sunt corporis eligendi temporibus, vero, fugiat optio eum voluptatum quam dignissimos quasi molestias dolor necessitatibus sapiente inventore? </p>
+            <br />
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit voluptas vel, saepe quia tempora suscipit quod exercitationem necessitatibus corporis distinctio tenetur maxime architecto eos dolorum veniam dignissimos maiores, obcaecati consectetur ut optio reprehenderit. Maiores velit repellat nam pariatur rem culpa molestiae itaque ut tempore voluptatibus sapiente ipsum quo amet accusantium, totam dolor? Nulla praesentium id architecto perspiciatis voluptatem quasi labore quam excepturi vel nostrum odio amet minus iusto adipisci sequi inventore sint eos esse distinctio quidem, laborum libero. Saepe suscipit soluta, tempora ex error iusto unde doloribus, reprehenderit nemo, officia ullam dolor dolore! Sed distinctio cupiditate cumque saepe. Quod consectetur suscipit et quos accusamus ab perferendis totam labore ipsam, ipsum earum neque, perspiciatis ullam sed aspernatur? Aliquam, nam. Ratione architecto itaque nostrum magni rerum fugit odio. Sequi illum numquam dignissimos deserunt, iste non! Sunt corporis eligendi temporibus, vero, fugiat optio eum voluptatum quam dignissimos quasi molestias dolor necessitatibus sapiente inventore? </p>
+        </div>
+    );
+};
+
+export default About;
